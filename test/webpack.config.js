89d9787da27a6073e18f5b872f05ba3ab7e63d8d
studzw <studzw@gmail.com>
2016-12-01 21:40:33 +0800

    fix lint

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -8,45 +8,45 @@ const SSICompileWebpackplugin = require('../index.js')
 
 
 const htmlArr = () => {
-	const html = glob.sync('./test/*.html')
-	let array = []
-	if(html.length > 0){
-		html.forEach((item, index, arr) => {
-			array.push(new HtmlWebpackPlugin({
-				filename: `${path.basename(item)}`,
-				template: item,
-				inject: true
-			}))
-		})
-	} 
-	return array
+    const html = glob.sync('./test/*.html')
+    let array = []
+    if(html.length > 0){
+        html.forEach((item, index, arr) => {
+            array.push(new HtmlWebpackPlugin({
+                filename: `${path.basename(item)}`,
+                template: item,
+                inject: true
+            }))
+        })
+    } 
+    return array
 }
 
 
 
 
 module.exports = {
-	entry: './test/entry.js',
-	output: {
-		path: `./test/release`,
-		filename: `test.js`
-	},
-	module:{
-		loaders: [
-			{	
-				test: /\.html$/,
-				loader: 'html?minimize=false'
-			}
-	    ]
-	},
-	plugins: [
-		new webpack.DefinePlugin({
-	        'process.env': {
-	    		NODE_ENV: '"production"'
-	        }
-	    }),
-	].concat(htmlArr(), new SSICompileWebpackplugin({
-		localBaseDir: path.resolve(__dirname, '..'),
-		publicPath: ''
-	}))
+    entry: './test/entry.js',
+    output: {
+        path: `./test/release`,
+        filename: `test.js`
+    },
+    module: {
+        loaders: [
+            {	
+                test: /\.html$/,
+                loader: 'html?minimize=false'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        })
+    ].concat(htmlArr(), new SSICompileWebpackplugin({
+        localBaseDir: path.resolve(__dirname, '..'),
+        publicPath: ''
+    }))
 }
