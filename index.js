192b2f4d6c51b89b72ab5269cd3a969ffedf6feb
Leonard Soai-Van <leo.soaivan@gmail.com>
2021-11-01 10:27:24 -0400

    Change indentation frm 4 to 2

--- index.js
+++ index.js
@@ -9,99 +9,96 @@ const getSource = require('./utils/get-source.js')
  * Replaces file/virtual tags in .*html files with online or local resources
  */
 class SSICompileWebpackplugin{
-    /**
-     * Creates an instance of SSICompileWebpackplugin.
-     * @param {String} publicPath Resource base path. If it is empty, the path will not be processed. If it is not empty, the path will be spliced into `${publicPath}/${path.basename}`
-     * @param {String} localBaseDir base directory for local files to be processed
-     * @param {String} ext File suffix to be processed. Use '\|' to separate multiple suffixes, such as '.html\|.shtml'
-     * @param {Boolean} minify Whether or not to compress HTML
-     */
-    constructor(options){
-        this.userOptions = options || {}
-        this.setting = Object.assign({}, {
-            publicPath: '',
-            localBaseDir: '/',
-            ext: '.html',
-            minify: false
-        }, options)
-    }
+	/**
+	 * Creates an instance of SSICompileWebpackplugin.
+	 * @param {String} publicPath Resource base path. If it is empty, the path will not be processed. If it is not empty, the path will be spliced into `${publicPath}/${path.basename}`
+	 * @param {String} localBaseDir base directory for local files to be processed
+	 * @param {String} ext File suffix to be processed. Use '\|' to separate multiple suffixes, such as '.html\|.shtml'
+	 * @param {Boolean} minify Whether or not to compress HTML
+	 */
+	constructor(options){
+		this.userOptions = options || {}
+		this.setting = Object.assign({}, {
+			publicPath: '',
+			localBaseDir: '/',
+			ext: '.html',
+			minify: false
+		}, options)
+	}
 
-    apply(compiler) {
-        const pluginName = 'SSICompileWebpackPlugin';
-        const { webpack } = compiler;
-        const { Compilation, sources: { RawSource } } = webpack;
+	apply(compiler) {
+		const pluginName = 'SSICompileWebpackPlugin';
+		const { webpack } = compiler;
+		const { Compilation, sources: { RawSource } } = webpack;
 
-        compiler.hooks.emit.tapAsync(pluginName, (compilation, callback) => {
-            const htmlArray = this.addFileToWebpackAsset(compilation);
-            const contents = htmlArray.map((filename) => this.replaceSSIFile(compilation, filename))
+		compiler.hooks.emit.tapAsync(pluginName, (compilation, callback) => {
+			const htmlArray = this.addFileToWebpackAsset(compilation);
+			const contents = htmlArray.map((filename) => this.replaceSSIFile(compilation, filename))
 
-            Promise.all(contents)
-                .then(() => callback())
-        })
-    }
+			Promise.all(contents)
+				.then(() => callback())
+		})
+	}
 
-    replaceSSIFile(compilation, filename){
-        const includeTagRegExp = /<!--#\s*include\s+(file|virtual)=(.*?)-->/g
-        let source = compilation.assets[filename].source()
-        const includeTags = source.match(includeTagRegExp)
+	replaceSSIFile(compilation, filename){
+		const includeTagRegExp = /<!--#\s*include\s+(file|virtual)=(.*?)-->/g
+		let source = compilation.assets[filename].source()
+		const includeTags = source.match(includeTagRegExp)
 
-        if(!includeTags){
-            return Promise.resolve(source)
-        }
+		if(!includeTags){
+			return Promise.resolve(source)
+		}
 
-        return Promise.all(includeTags.map((item) => {
-            const src = item.split('"')[1]
-            return getSource(src, this.userOptions)
-        }))
-            .then((results) => {
-                includeTags.forEach((tag, index) => {
-                    source = source.replace(tag, (matchItem) => (
-                        decodeURIComponent(matchItem = encodeURIComponent(results[index]))
-                    ))
-                })
+		return Promise.all(includeTags.map((item) => {
+			const src = item.split('"')[1]
+			return getSource(src, this.userOptions)
+		}))
+			.then((results) => {
+				includeTags.forEach((tag, index) => {
+					source = source.replace(tag, (matchItem) => (
+						decodeURIComponent(matchItem = encodeURIComponent(results[index]))
+					))
+				})
 
-                compilation.assets[filename].source = () => {
-                    return this.userOptions.minify ? minify(source, {
-                        collapseWhitespace: true,
-                        minifyCSS: true,
-                        minifyJS: true
-                    }) : source
-                }
+				compilation.assets[filename].source = () => {
+					return this.userOptions.minify ? minify(source, {
+						collapseWhitespace: true,
+						minifyCSS: true,
+						minifyJS: true
+					}) : source
+				}
 
-                return results
-            })
-            .catch(err => {
-                console.log('ERROR: ', err)
-            })
-    }
+				return results
+			})
+			.catch(err => {
+				console.log('ERROR: ', err)
+			})
+	}
 
-    addFileToWebpackAsset(compilation){
-        const htmlName = []
-        const source = compilation.assets
+	addFileToWebpackAsset(compilation){
+		const htmlName = []
+		const source = compilation.assets
 
-        Object.keys(source).forEach((item, index, array) => {
-            let extReg = new RegExp(this.userOptions.ext, 'g')
-            if (extReg.test(item)) {
-                htmlName.push(item)
-                compilation.fileDependencies.add(item)
+		Object.keys(source).forEach((item, index, array) => {
+			let extReg = new RegExp(this.userOptions.ext, 'g')
+			if (extReg.test(item)) {
+				htmlName.push(item)
+				compilation.fileDependencies.add(item)
 
-                const str = source[item].source()
-                compilation.assets[item] = {
-                    source: function() {
-                        return str
-                    },
-                    size: function() {
-                        return str.length
-                    }
-                }
-            }
-        })
+				const str = source[item].source()
+				compilation.assets[item] = {
+					source: function() {
+						return str
+					},
+					size: function() {
+						return str.length
+					}
+				}
+			}
+		})
 
-        return htmlName
-    }
-
-
-
+		return htmlName
+	}
 }
 
 module.exports = SSICompileWebpackplugin
